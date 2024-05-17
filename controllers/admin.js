b0702f25def0560c55e58228ce34009f60320c5e
Jonas Arvad Kristiansen <jonasarvad2@gmail.com>
2024-05-17 14:07:09 +0200

    admin try catch done

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,27 +4,27 @@ const allData = (req, res) => {
     //promise all makes it so that if all is success it will send the data to frontend, but if one fails it returns error
     Promise.all([
         new Promise((resolve, reject) => {
-            db.query('SELECT COUNT(*) AS count FROM companys', (err, result) => {
-                if (err) {
-                    console.error(err);
+            db.query('SELECT COUNT(*) AS count FROM companys', (error, result) => {
+                if (error) {
+                    reject(error)
                 } else {
                     resolve(result[0].count);
                 }
             });
         }),
         new Promise((resolve, reject) => {
-            db.query('SELECT COUNT(*) AS count FROM users WHERE isAdmin = 0', (err, result) => {
-                if (err) {
-                    console.error(err);
+            db.query('SELECT COUNT(*) AS count FROM users WHERE isAdmin = 0', (error, result) => {
+                if (error) {
+                    reject(error)
                 } else {
                     resolve(result[0].count);
                 }
             });
         }),
         new Promise((resolve, reject) => {
-            db.query('SELECT COUNT(*) AS count FROM jobpostings', (err, result) => {
-                if (err) {
-                    console.error(err);
+            db.query('SELECT COUNT(*) AS count FROM jobpostings', (error, result) => {
+                if (error) {
+                    reject(error)
                 } else {
                     resolve(result[0].count);
                 }
@@ -34,18 +34,18 @@ const allData = (req, res) => {
         .then(([companyCount, usersCount, jobpostingCount]) => {
             return res.status(200).json({ countOfcompanys: companyCount, countOfUser: usersCount, countOfJobpostings: jobpostingCount });
         })
-        .catch((err) => {
-            console.error(err);
-            return res.status(500).json('Server fejl');
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json('Kunne ikke hente statistik data');
         });
 };
 
 // for getting all banned emails
 const getAllBannedEmails = (req, res) => {
-    db.query('SELECT email FROM bannedemails', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json('server fejl');
+    db.query('SELECT email FROM bannedemails', (error, data) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json('Kunne ikke hente ban emails');
         } else {
             return res.status(200).json({ emails: data });
         }
@@ -55,15 +55,14 @@ const getAllBannedEmails = (req, res) => {
 // this is used to check if email is already banned
 const bannedEmailCheck = (email) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM bannedemails WHERE email = ?', email, (err, data) => {
-            if (err) {
-                console.error(err);
-                resolve({ success: false });
+        db.query('SELECT * FROM bannedemails WHERE email = ?', email, (error, data) => {
+            if (error) {
+                reject({ error: error, errorMessage: 'Kunne ikke tjekke ban email' });
             } else {
                 if (data.length > 0) {
-                    resolve({ success: false });
+                    reject({ errorMessage: 'Ban email eksitere allerede' });
                 } else {
-                    resolve({ success: true });
+                    resolve(true);
                 }
             }
         });
@@ -73,14 +72,13 @@ const bannedEmailCheck = (email) => {
 // for ban a email from use
 const banEmail = (email) => {
     return new Promise((resolve, reject) => {
-        db.query('INSERT INTO bannedemails (email) VALUES (?)', email, (err, result) => {
-            if (err) {
-                console.error(err);
-                resolve({ success: false });
+        db.query('INSERT INTO bannedemails (email) VALUES (?)', email, (error, result) => {
+            if (error) {
+                reject({ error: error, errorMessage: 'Kunne ikke ban email' });
             } else if (result.affectedRows == 0) {
-                resolve({ success: false });
+                reject({ errorMessage: 'Kunne ikke ban email' });
             } else {
-                resolve({ success: true });
+                resolve(true);
             }
         });
     });
@@ -89,14 +87,13 @@ const banEmail = (email) => {
 // for deleting a user with admin permissions
 const deleteUser = (userId) => {
     return new Promise((resolve, reject) => {
-        db.query('DELETE from users WHERE id = ?', userId, (err, result) => {
-            if (err) {
-                console.error(err);
-                resolve({ success: false });
+        db.query('DELETE from users WHERE id = ?', userId, (error, result) => {
+            if (error) {
+                reject({ error: error, errorMessage: 'Brugerens profil kunne ikke slettes eller brugerens profil kunne ikke findes' });
             } else if (result.affectedRows == 0) {
-                resolve({ success: false });
+                reject({ errorMessage: 'Brugerens profil kunne ikke slettes eller brugerens profil kunne ikke findes' });
             } else {
-                resolve({ success: true });
+                resolve(true);
             }
         });
     });
@@ -105,12 +102,11 @@ const deleteUser = (userId) => {
 // for deleting a company with admin permissions
 const deleteCompany = (companyId) => {
     return new Promise((resolve, reject) => {
-        db.query('DELETE from companys WHERE id = ?', companyId, (err, result) => {
-            if (err) {
-                console.error(err);
-                resolve({ success: false });
+        db.query('DELETE from companys WHERE id = ?', companyId, (error, result) => {
+            if (error) {
+                reject({ error: error, errorMessage: 'Virksomheds brugeren kunne ikke slettes eller virksomheds brugeren kunne ikke findes' });
             } else if (result.affectedRows == 0) {
-                resolve({ success: false });
+                reject({ errorMessage: 'Virksomheds brugeren kunne ikke slettes eller virksomheds brugeren kunne ikke findes' });
             } else {
                 resolve({ success: true });
             }
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -23,72 +23,89 @@ router.get('/allBannedEmails', async (req, res) => {
 });
 
 router.post('/banEmail', async (req, res) => {
-    const { email } = req.body;
+    try {
+        const { email } = req.body;
 
-    if (!email) {
-        return res.status(400).json('email mangler');
-    }
-
-    let jwtVerify = await jwt.verifyToken(req);
-
-    if (jwtVerify.type != 'Admin') {
-        return res.status(401).json('Ikke tilladt at ban. Kun admin har tilladelse');
-    }
-
-    let checkEmail = await admin.bannedEmailCheck(email);
-    if (checkEmail.success) {
-        let result = await admin.banEmail(email);
-        if (result.success) {
-            return res.status(200).json('Email kan længere bruges på siden');
-        } else {
-            return res.status(500).json('Server fejl. Kunne ikke forbyde email');
+        if (!email) {
+            return res.status(400).json('email mangler');
         }
-    } else {
-        return res.status(409).json('Ban email eksitere allerede');
+
+        let jwtVerify = await jwt.verifyToken(req);
+
+        if (jwtVerify.type != 'Admin') {
+            return res.status(401).json('Ikke tilladt at ban. Kun admin har tilladelse');
+        }
+
+        let checkEmail = await admin.bannedEmailCheck(email);
+
+        if (checkEmail) {
+            let result = await admin.banEmail(email);
+            
+            if (result) {
+                return res.status(200).json('Email kan længere bruges på siden');
+            } else {
+                return res.status(500).json('Server fejl. Kunne ikke forbyde email');
+            }
+        } else {
+            return res.status(409).json('Ban email eksitere allerede');
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error.errorMessage);
     }
 });
 
 router.delete('/deleteUser', async (req, res) => {
-    const { userId } = req.body;
+    try {
+        const { userId } = req.body;
 
-    if (!userId) {
-        return res.status(400).json('userId mangler');
-    }
+        if (!userId) {
+            return res.status(400).json('userId mangler');
+        }
 
-    let jwtVerify = await jwt.verifyToken(req);
+        let jwtVerify = await jwt.verifyToken(req);
 
-    if (jwtVerify.type != 'Admin') {
-        return res.status(401).json('Ikke tilladt at slette brugere. Kun admin har tilladelse');
-    }
+        if (jwtVerify.type != 'Admin') {
+            return res.status(401).json('Ikke tilladt at slette brugere. Kun admin har tilladelse');
+        }
 
-    let result = await admin.deleteUser(req.body.userId);
+        let result = await admin.deleteUser(req.body.userId);
 
-    if (result.success) {
-        return res.status(200).json('Brugerens profil er slettet');
-    } else {
-        return res.status(500).json('Brugerens profil kunne ikke slettes eller brugerens profil kunne ikke findes');
+        if (result) {
+            return res.status(200).json('Brugerens profil er slettet');
+        } else {
+            return res.status(500).json('Brugerens profil kunne ikke slettes eller brugerens profil kunne ikke findes');
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error.errorMessage);
     }
 });
 
 router.delete('/deleteCompany', async (req, res) => {
-    const { companyId } = req.body;
+    try {
+        const { companyId } = req.body;
 
-    if (!companyId) {
-        return res.status(400).json('companyId mangler');
-    }
+        if (!companyId) {
+            return res.status(400).json('companyId mangler');
+        }
 
-    let jwtVerify = await jwt.verifyToken(req);
+        let jwtVerify = await jwt.verifyToken(req);
 
-    if (jwtVerify.type != 'Admin') {
-        return res.status(401).json('Ikke tilladt at slette virksomheds brugere. Kun admin har tilladelse');
-    }
+        if (jwtVerify.type != 'Admin') {
+            return res.status(401).json('Ikke tilladt at slette virksomheds brugere. Kun admin har tilladelse');
+        }
 
-    let result = await admin.deleteCompany(req.body.companyId);
+        let result = await admin.deleteCompany(req.body.companyId);
 
-    if (result.success) {
-        return res.status(200).json('Virksomheds bruger profil er slettet');
-    } else {
-        return res.status(500).json('Virksomheds brugeren kunne ikke slettes eller virksomheds brugeren kunne ikke findes');
+        if (result.success) {
+            return res.status(200).json('Virksomheds bruger profil er slettet');
+        } else {
+            return res.status(500).json('Virksomheds brugeren kunne ikke slettes eller virksomheds brugeren kunne ikke findes');
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json(error.errorMessage);
     }
 });
 
