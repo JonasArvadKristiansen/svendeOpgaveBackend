41bdb66037550f5f2cd995522e1a3c64d6fbab31
Jonas Arvad Kristiansen <jonasarvad2@gmail.com>
2024-05-28 13:14:11 +0200

    change timezone to avoid error on getting right date out

--- utils/DB.js
+++ utils/DB.js
@@ -10,6 +10,7 @@ const con = mysql
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         connectionLimit: process.env.DB_CONNECTIONLIMIT,
+        timezone: 'Z'
     })
     .promise();
 /* by using promise on connection, makes so the query can be awaited and 
