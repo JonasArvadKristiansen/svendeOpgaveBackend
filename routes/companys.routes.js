f9a2aae84d86d84787dc31ebbec046eb6e33120d
Jonas Arvad Kristiansen <jonasarvad2@gmail.com>
2024-06-02 20:49:23 +0200

    fixed error

--- routes/companys.routes.js
+++ routes/companys.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const companys = require('../controllers/companys');
 const jwt = require('../utils/jwt');
-const loginLimit = require('../utils/loginlimter');
+const loginLimit = require('../utils/loginlimiter');
 
 router.get('/all', async (req, res, next) => {
     try {
